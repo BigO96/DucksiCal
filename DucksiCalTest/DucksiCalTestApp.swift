e22147263579df4ef3e0856585bbe3f653364261
Oscar Epp <oepp@uoregon.edu>
2024-04-24 21:02:25 -0700

    Initial Commit

---
+++ DucksiCalTest/DucksiCalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DucksiCalTestApp.swift
+//  DucksiCalTest
+//
+//  Created by Oscar Epp on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct DucksiCalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
